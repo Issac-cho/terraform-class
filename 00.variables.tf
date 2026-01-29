73495ff4b502d9032804433e44887a28f2bf8849
JUNHEE CHO <cho1004jh@gmail.com>
2026-01-29 12:43:50 +0900

    Add files via upload

---
+++ 00.variables.tf
@@ -0,0 +1,12 @@
+variable "AWS_ACCESS_KEY_ID" {
+  description = "AKIAQYM3OQF6KQHQTP4A"
+  type        = string
+  default     = ""
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "Kc0hWBXkW8hOV2ajO2TlaJgQT+wKLXcjS6V3Ai0T"
+  type        = string
+  sensitive   = true
+  default     = ""
+}
