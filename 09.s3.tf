818c2bb8d4cb1c59707706a46b8e05950917fe7b
JUNHEE CHO <cho1004jh@gmail.com>
2026-01-29 14:47:14 +0900

    Rename S3 bucket resource to terraform-issac-cho-123456

--- 09.s3.tf
+++ 09.s3.tf
@@ -1,4 +1,5 @@
-resource "aws_s3_bucket" "terraform-issac-cho-20260129" {
-  bucket = "terraform-issac-cho-20260129"
+resource "aws_s3_bucket" "terraform-issac-cho-123456" {
+  bucket = "terraform-issac-cho-123456"
 
 }
+
